97adcb9e4414ba6a7324f68471e2e5056cb187a3
DarshanLoni <132261307+DarshanLoni@users.noreply.github.com>
2025-03-18 19:49:21 +0530

    Delete newfile.cpp directory

--- newfile.cpp/newfile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, PES\n";
-  cout << "Hello, I Am PES2UG22CS158\n";
-  cout << "I have successfully done with Jenkins lab\n";
-  return 0;
-}
